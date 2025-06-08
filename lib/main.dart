dcea82246f73fec280c696cf698f2d57e4363e11
Sharini <161706694+ShariniN@users.noreply.github.com>
2025-06-08 08:05:12 +0530

    Adding images to the pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Apple Store UI',
+      title: 'Electronics Store',
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.blue,
